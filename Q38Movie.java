b80bbdad875dc76740b87d305ad9ec2a97d82219
pankajpatwa42 <pankajpatwa42@gmail.com>
2022-06-08 13:20:39 +0530

    [Update] impleents getPg Func

--- Q38Movie.java
+++ Q38Movie.java
@@ -1,5 +1,43 @@
 package Assignment0606;
 
 public class Q38Movie {
+	private String title;
+	private String studio;
+	private String rating;
+	
+	public Q38Movie(String title,String studio,String rating)
+	{
+		this.title = title;
+		this.rating=rating;
+		this.studio = studio;
+	}
+	public Q38Movie(String title,String studio)
+	{
+		this.rating = "PG";
+		this.title = title;
+		this.studio = studio;
+	}
+	public Q38Movie[] getPg (Q38Movie[] mov)
+	{
+		Q38Movie[] pgMov = new Q38Movie[mov.length];
+		int newArrayIndex = 0;
+		for(int i =0;i<mov.length;i++)
+		{
+			if(mov[i].rating.equals("PG")) {
+				pgMov[newArrayIndex]=mov[i];
+				newArrayIndex++;
+			}	
+		}
+		return pgMov;
+	}
+	
+	public static void main(String args[])
+	{
+		
+		}
 
-}
+		
+		
+	}		
+
+
